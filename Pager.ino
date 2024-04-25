38f75be09355a58312eb244c1aff1101b54560cd
VeryLongNicknameSuchWow <d33p4ck@gmail.com>
2024-04-25 17:03:02 +0200

    Server polling

--- Pager.ino
+++ Pager.ino
@@ -1,6 +1,8 @@
 #include <TFT_eSPI.h>
 #include <ReactESP.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 #include "ScrollingLine.h"
 #include "Settings.h"
 
@@ -71,6 +73,88 @@ removeReaction:
   });
 }
 
+JsonDocument jsonDocument;
+String serializedJsonDocument;
+int pingServer() {
+  HTTPClient http;
+  http.begin(serverUrl);
+  http.addHeader("Content-Type", "application/json");
+
+  jsonDocument.clear();
+  jsonDocument["mac_address"] = WiFi.macAddress();
+  JsonArray scanResults = jsonDocument.createNestedArray("scan_results");
+
+  int n = WiFi.scanNetworks();
+  for (int i = 0; i < n; ++i) {
+    JsonObject scanResult = scanResults.createNestedObject();
+    scanResult["ssid"] = WiFi.SSID(i);
+    scanResult["rssi"] = WiFi.RSSI(i);
+    scanResult["bssid"] = WiFi.BSSIDstr(i);
+  }
+
+  serializeJson(jsonDocument, serializedJsonDocument);
+  Serial.print("HTTP POST body: ");
+  Serial.println(serializedJsonDocument);
+
+  int httpResponseCode = http.POST(serializedJsonDocument);
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response ");
+    Serial.print(httpResponseCode);
+    Serial.print(": ");
+    serializedJsonDocument = http.getString();
+    Serial.println(serializedJsonDocument);
+    deserializeJson(jsonDocument, serializedJsonDocument);
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+
+  return httpResponseCode;
+}
+
+void connect() {
+  static RepeatReaction* wifiConnectionReaction = nullptr;
+  static int failedConnectionCount = 0;
+  const int maxFailedConnections = 5;
+
+  if (wifiConnectionReaction != nullptr) {
+    return;
+  }
+
+  Serial.println("wifiConnectionReaction initialized");
+  lines[1].setText("Connecting...");
+  WiFi.begin(ssid, password);
+
+  wifiConnectionReaction = app.onRepeat(1000, [&]() {
+    Serial.println("wifiConnectionReaction is running");
+
+    if (WiFi.status() != WL_CONNECTED) {
+      failedConnectionCount++;
+      if (failedConnectionCount > maxFailedConnections) {
+        lines[1].setText("Connection failed");
+        goto removeReaction;
+      }
+      return;
+    }
+
+    lines[1].setText("Connected");
+    pingServer();
+
+    // TODO: do things according to server response
+    // for now just initialize a scan :v
+    app.onDelay(0, scanAndShow);
+
+removeReaction:
+    app.remove(wifiConnectionReaction);
+    wifiConnectionReaction = nullptr;
+    failedConnectionCount = 0;
+    Serial.println("wifiConnectionReaction removed");
+    lines[1].setText("Disconnected");
+
+    WiFi.disconnect();
+  });
+}
 
 void setup() {
   Serial.begin(115200);
@@ -89,8 +173,8 @@ void setup() {
   lines[2].setText("");
 
   app.onRepeat(20, scrollAllLines);
-  app.onDelay(0, scanAndShow);
-  app.onRepeat(30000, scanAndShow);
+  app.onDelay(0, connect);
+  app.onRepeat(30000, connect);
 }
 
 void loop() {
